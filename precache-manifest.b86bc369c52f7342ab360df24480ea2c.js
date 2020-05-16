19f203888015db52947541b926ad44394199c1cd
Anastasia Lukashyna <L_A_A_93@mail.ru>
2020-05-16 20:48:54 +0300

    Updates

---
+++ precache-manifest.b86bc369c52f7342ab360df24480ea2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ee4f0d1989a27c6151abffe29c0250",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "75b6ea3e3eaec5913dd3",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.1497ee27.chunk.css"
+  },
+  {
+    "revision": "cd0dc95069f3aaffe92c",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.c014719f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.c014719f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b6ea3e3eaec5913dd3",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.7b37ca0c.chunk.js"
+  },
+  {
+    "revision": "5da324139d8150755543",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d2f0dcbe.js"
+  }
+]);
